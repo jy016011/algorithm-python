40fdf8388b462a70bb0453ba78b083208b7c3bf7
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-03-03 22:50:39 +0900

    240303 solved

---
+++ programmers/high_score_kit/greedy/_Q1_Lv1.py
@@ -0,0 +1,32 @@
+def solution(n, lost, reserve):
+    answer = n
+    lost.sort()
+    reserve.sort()
+    visited = [False] * (n + 1)
+    for l in lost:
+        for r in reserve:
+            if l == r:
+                visited[r] = True
+                break
+    for l in lost:
+        if not visited[l]:
+            isBorrowed = False
+            for r in reserve:
+                if (l - 1 == r or l + 1 == r) and not visited[r]:
+                    visited[r] = True
+                    isBorrowed = True
+                    break
+            if not isBorrowed:
+                answer -= 1
+
+    return answer
+
+
+print(solution(5, [2, 4], [1, 3, 5]) == 5)
+print(solution(5, [2, 4], [3]) == 4)
+print(solution(3, [3], [1]) == 2)
+print(solution(
+    30,
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 30
+      )
