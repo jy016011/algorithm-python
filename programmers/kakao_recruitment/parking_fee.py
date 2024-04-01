40e25a3a92c2132453cf47ea22d48938c8bff5df
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-04-01 23:48:15 +0900

    240401 solved

--- programmers/kakao_recruitment/parking_fee.py
+++ programmers/kakao_recruitment/parking_fee.py
@@ -35,3 +35,21 @@ def calculate_parking_time(time_record, fee_record, car_no, time):
 
 def to_minute(hour, minute):
     return int(hour) * 60 + int(minute)
+
+
+print(
+    solution(
+        [180, 5000, 10, 600],
+        [
+            "05:34 5961 IN",
+            "06:00 0000 IN",
+            "06:34 0000 OUT",
+            "07:59 5961 OUT",
+            "07:59 0148 IN",
+            "18:59 0000 IN",
+            "19:09 0148 OUT",
+            "22:59 5961 IN",
+            "23:00 5961 OUT"
+        ]
+    ) == [14600, 34400, 5000]
+)
